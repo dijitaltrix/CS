247eec290e784a9d12d49470d16b451271b5ee12
Ian Grindley <ian@ignitionstudios.co>
2019-05-27 19:08:08 +0100

    Support older versions of MySQL
    Change default string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+		// support older versions of MySQL/MariaDB
+		// https://laravel.com/docs/master/migrations#creating-indexes
+        Schema::defaultStringLength(191);
     }
 }
